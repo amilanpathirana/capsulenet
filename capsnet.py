33552fb7c5aaa5c9757bd14e8925394d29b45b97
Erik Reppel <erikreppel@gmail.com>
2017-11-16 21:09:40 -0800

    Clean up and better comment loss function

--- capsnet.py
+++ capsnet.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 from torch.autograd import Variable
 
 from capsule_layer import CapsuleLayer
@@ -21,48 +22,62 @@ class CapsNet(nn.Module):
                                     False,
                                     n_routing_caps)
 
-        self.routing_caps = CapsuleLayer(n_primary_caps,
-                                         primary_cap_size,
-                                         10,  # 10 catagories in MNIST
-                                         output_unit_size,
-                                         True,
-                                         n_routing_caps)
+        self.final_caps = CapsuleLayer(n_primary_caps,
+                                       primary_cap_size,
+                                       10,  # 10 catagories in MNIST
+                                       output_unit_size,
+                                       True,
+                                       n_routing_caps)
 
     def forward(self, X):
         X = self.conv1(X)
         X = self.primary(X)
-        X = self.routing_caps(X)
+        X = self.final_caps(X)
         return X
 
-    def loss(self, input, target, size_average=True):
-        """Custom loss function"""
-        m_loss = self.margin_loss(input, target, size_average)
-        return m_loss
-
-    def margin_loss(self, input, target, size_average=True):
-        """Margin loss for digit existence
+    def loss(self, pred, target, size_average=True):
         """
-        batch_size = input.size(0)
+        Margin loss for digit existence described in eq.4
+        params:
+        - pred: output from digit caps (16x10 for mnist)
+        - target: one-hot encoding of class
+        """
+        batch_size = pred.size(0)
 
-        # Implement equation 4 in the paper.
+        # ||v_k||
+        v_k = torch.sqrt((pred**2).sum(dim=2, keepdim=True))
 
-        # ||vc||
-        v_c = torch.sqrt((input**2).sum(dim=2, keepdim=True))
-
-        # Calculate left and right max() terms.
         zero = Variable(torch.zeros(1))
-        zero = zero.cuda() if input.is_cuda else zero
+        zero = zero.cuda() if pred.is_cuda else zero
+
+        # constants defined in paper
         m_plus = 0.9
         m_minus = 0.1
         loss_lambda = 0.5
-        max_left = torch.max(m_plus - v_c, zero).view(batch_size, -1)
-        max_right = torch.max(v_c - m_minus, zero).view(batch_size, -1)
-        t_c = target
-        # Lc is margin loss for each digit of class c
-        l_c = t_c * max_left + loss_lambda * (1.0 - t_c) * max_right
-        l_c = l_c.sum(dim=1)
+
+        # Tk max(0, m+ − ||v_k||)^2 (left side of eq 4)
+        left = torch.max(zero, m_plus - v_k).view(batch_size, -1)
+
+        # λ (1 − T_k) max(0, ||vk|| − m_minus)^2 (right side of eq 4)
+        right = torch.max(zero, v_k - m_minus).view(batch_size, -1)
+
+        t_k = target
+
+        # L_k is margin loss for each digit of class k
+        l_k = t_k * left + loss_lambda * (1.0 - t_k) * right
+        l_k = l_k.sum(dim=1)
 
         if size_average:
-            l_c = l_c.mean()
+            l_k = l_k.mean()
 
-        return l_c
+        return l_k
+
+    def capsule_prediction(self, final_caps):
+        '''
+        Converts output of last caps layer to softmax probability of each digit
+        params:
+        - final_caps: output from final layer, in mnist the shape is (16, 10)
+        '''
+        # ||D|| where D is the digit caps
+        return F.softmax(torch.sum(final_caps**2,
+                                   dim=2, keepdim=True)).view(10)
